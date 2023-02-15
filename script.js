b3978fe24e57b6af625cf89a7eeb9acb2d5fe031
Hyqerion199 <78224761+Hyqerion199@users.noreply.github.com>
2023-02-14 20:55:32 -0500

    put in existing code

---
+++ script.js
@@ -0,0 +1,74 @@
+function checkPassword() {
+  var passwordInput = document.getElementById("password");
+  var password = btoa(passwordInput.value); // encode the password in base64
+
+  var correctPassword = btoa("test")
+  if (password === correctPassword) {
+    document.getElementById("login-box").style.display = "none";
+    document.getElementById("game-list").style.display = "block";
+  } else {
+    document.getElementById("error-message").style.display = "block";
+  }
+}
+
+function checkPWD(e) {
+  if (e.keyCode === 13) {
+    checkPassword()
+  }
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+window.addEventListener('keydown', checkPWD);
+
+
+/**
+function darkmode() {
+  document.getElementById("darkmode").onclick = function() {
+
+    if (localStorage.getItem("darkmode") === "darkmode") {
+      localStorage.setItem("darkmode", "lightmode");
+    } else {
+      localStorage.setItem("darkmode", "darkmode");
+    }
+  }
+
+  setInterval(function() {
+    var element = document.body;
+    if (localStorage.getItem("darkmode") === "darkmode") {
+      try {
+        element.classList.add("dark-mode");
+      } catch (error) {
+
+      }
+    } else if (localStorage.getItem("darkmode") !== "darkmode") {
+      try {
+        element.classList.remove("dark-mode");
+      } catch (error) {
+
+      }
+    };
+  }, 400)
+}
+
+darkmode()
+
+async function getname() {
+  const supautok = localStorage.getItem('supabase.auth.token');
+  const supapar = JSON.parse(supautok);
+  const supapar2 = JSON.stringify(JSON.parse(JSON.stringify(supapar.currentSession)));
+  const supapar3 = JSON.parse(supapar2);
+
+  const { data, error } = await _supabase.auth.api.getUser(supapar3.access_token);
+  if (!error) {
+    var yooo = `${data.user_metadata.first_name} ${data.user_metadata.last_name}`;
+    return yooo;
+  }
+}
+
+
+checkkliv = getname()
+checkkliv.then((name) => {
+  if (name === "") { window.location.replace("/401/"); }
+})
+
+ */
