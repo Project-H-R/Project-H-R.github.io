14f65f02f3956ccd852d9600f7400f5d7b39a556
Hyqerion199 <matjel08@gmail.com>
2023-03-08 21:25:48 -0500

    some fun changes

---
+++ ai/myscript.js
@@ -0,0 +1,35 @@
+function chat() {
+    const url = 'https://api.openai.com/v1/engines/' + document.getElementById('model').value + '/completions';
+    const api_key = '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';
+    const prompt = document.getElementById('prompt').value;
+    const temperature = 1;
+    const max_tokens = 3750;
+    document.getElementById('prompt').value = "";
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${api_key}`
+      },
+      body: JSON.stringify({
+        prompt,
+        temperature,
+        max_tokens
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const responseElement = document.getElementById('response');
+      const generatedText = data.choices[0].text.replace(/\n/g, '<br>');
+      responseElement.innerHTML = generatedText;
+    })
+    .catch(error => console.error(error));
+  }
+
+  const promptInput = document.getElementById('prompt');
+  promptInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      document.querySelector('button').click();
+    }
+  });
